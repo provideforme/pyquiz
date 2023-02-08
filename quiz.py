34ec84284b5d1673c4ade059d6ce21088e71309e
provideforme <evan.a.bernard@gmail.com>
2023-02-08 16:32:17 -0500

    upgrade to python3.11

--- quiz.py
+++ quiz.py
@@ -5,34 +5,34 @@ from string import ascii_lowercase
 
 NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
-  "When was the first known use of the word 'quiz'": [
-      "1781", "1771","1871", "1881"
-  ],
-  "Which built in function can get information from the user": [
-      "input", "get", "print", "write"
-  ],
-  "Which Keyword do you use to loop over a given list of elements": [
-      "for", "while", "each", "loop"
-  ],
-  "What's the purpose of the built-in zip() function": [
+    "When was the first known use of the word 'quiz'": [
+        "1781", "1771","1871", "1881"
+    ],
+    "Which built in function can get information from the user": [
+        "input", "get", "print", "write"
+    ],
+    "Which Keyword do you use to loop over a given list of elements": [
+        "for", "while", "each", "loop"
+    ],
+    "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
         "To compress several files into one archive",
         "To get information from the user",
     ],
-  "What's the name of Python's sorting algorithm": [
+    "What's the name of Python's sorting algorithm": [
         "Timesort", "Quicksort", "Merge sort", "Bubble sort"
     ],
-  "What does dict.get(key) return if key isn't found in dict": [
+    "What does dict.get(key) return if key isn't found in dict": [
         "None", "key", "True", "False",
     ],
-  "What's the official name of the := operator": [
+    "What's the official name of the := operator": [
         "Assignment expression",
         "Named expression",
         "Walrus operator",
         "Colon equals operator",
     ],
-  "What's one effect of calling random.seed(42)": [
+    "What's one effect of calling random.seed(42)": [
         "The random numbers are reproducible.",
         "The random numbers are more random.",
         "The computer clock is reset.",
@@ -62,6 +62,6 @@ for num, (question, alternatives) in enumerate(questions, start=1):
         num_correct += 1
         print("⭐ Correct! ⭐")
     else:
-      print(f"The answer is {correct_answer!r}, not {answer!r}")
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
 
 print(f"\nYou got {num_correct} correct out of {num} questions")
--- refactor.py
+++ refactor.py
@@ -5,40 +5,40 @@ from string import ascii_lowercase
 
 NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
-  "When was the first known use of the word 'quiz'": [
-      "1781", "1771","1871", "1881"
-  ],
-  "Which built in function can get information from the user": [
-      "input", "get", "print", "write"
-  ],
-  "Which Keyword do you use to loop over a given list of elements": [
-      "for", "while", "each", "loop"
-  ],
-  "What's the purpose of the built-in zip() function": [
+    "When was the first known use of the word 'quiz'": [
+        "1781", "1771","1871", "1881"
+    ],
+    "Which built in function can get information from the user": [
+        "input", "get", "print", "write"
+    ],
+    "Which Keyword do you use to loop over a given list of elements": [
+        "for", "while", "each", "loop"
+    ],
+    "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
         "To compress several files into one archive",
         "To get information from the user",
     ],
-  "What's the name of Python's sorting algorithm": [
+    "What's the name of Python's sorting algorithm": [
         "Timesort", "Quicksort", "Merge sort", "Bubble sort"
     ],
-  "What does dict.get(key) return if key isn't found in dict": [
+    "What does dict.get(key) return if key isn't found in dict": [
         "None", "key", "True", "False",
     ],
-  "What's the official name of the := operator": [
+    "What's the official name of the := operator": [
         "Assignment expression",
         "Named expression",
         "Walrus operator",
         "Colon equals operator",
     ],
-  "What's one effect of calling random.seed(42)": [
+    "What's one effect of calling random.seed(42)": [
         "The random numbers are reproducible.",
         "The random numbers are more random.",
         "The computer clock is reset.",
         "The first random number is always 42.",
     ],
-  "When does __name__ == '__main__' equal True in a Python file": [
+    "When does __name__ == '__main__' equal True in a Python file": [
         "When the file is run as a script",
         "When the file is imported as a module",
         "When the file has a valid name",
@@ -48,7 +48,7 @@ QUESTIONS = {
 
 def run_quiz():
     questions = prepare_questions(
-      QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
+        QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ
     )
 
     num_correct = 0
