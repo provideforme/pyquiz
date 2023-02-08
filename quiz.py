9359265394adb3fe34d4b98ee05356ccb266d3fc
provideforme <evan.a.bernard@gmail.com>
2023-02-08 12:28:15 -0500

    part 5

--- quiz.py
+++ quiz.py
@@ -16,14 +16,19 @@ QUESTIONS = {
         "To compress several files into one archive",
         "To get information from the user",
     ],
+  "What's the name of Python's sorting algorithm": [
+        "Timsort", "Quicksort", "Merge sort", "Bubble sort"
+    ],
 }
 
 for question, alternatives in QUESTIONS.items():
     correct_answer = alternatives[0]
-    for alternative in sorted(alternatives):
-      print(f"  - {alternative}")
+    sorted_alternatives = sorted(alternatives)
+    for label, alternative in enumerate(sorted_alternatives):
+      print(f"  {label}) {alternative}")
 
-    answer = input(f"{question}? ")
+    answer_label = int(input(f"{question}? "))
+    answer = sorted_alternatives[answer_label]
     if answer == correct_answer:
         print("Correct!")
     else:
