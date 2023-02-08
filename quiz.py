5d8af029e22e888f84a10fec7659dc746ec49eb5
provideforme <evan.a.bernard@gmail.com>
2023-02-08 12:40:08 -0500

    part 6

--- quiz.py
+++ quiz.py
@@ -1,5 +1,7 @@
 # quiz.py 
 
+from string import ascii_lowercase
+
 QUESTIONS = {
   "When was the first known use of the word 'quiz'": [
       "1781", "1771","1871", "1881"
@@ -19,17 +21,22 @@ QUESTIONS = {
   "What's the name of Python's sorting algorithm": [
         "Timsort", "Quicksort", "Merge sort", "Bubble sort"
     ],
+  "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ]
 }
 
-for question, alternatives in QUESTIONS.items():
+for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-    for label, alternative in enumerate(sorted_alternatives):
-      print(f"  {label}) {alternative}")
+    labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
 
-    answer_label = int(input(f"{question}? "))
-    answer = sorted_alternatives[answer_label]
+    answer_label = input(f"\nChoice? ")
+    answer = labeled_alternatives.get(answer_label)
     if answer == correct_answer:
-        print("Correct!")
+        print("⭐ Correct! ⭐")
     else:
       print(f"The answer is {correct_answer!r}, not {answer!r}")
